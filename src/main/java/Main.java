7f9209bb0659b16730eca1b4000771aa37116b4b
kjartanLeander <Kjar0321@stud.kea.dk>
2022-09-14 11:00:23 +0200

    added "list over helte"

---
+++ src/main/java/Main.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[]args){
+        Database database =new Database();
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("-----------------------------");
+        do  {
+
+            System.out.println("Velkommen til MySuperheroList");
+            System.out.println("1. Opret Superhelte");
+            System.out.println("5. Liste over helte");
+            System.out.println("9. Luk programmet");
+            int startside = scanner.nextInt();
+
+            if (startside==1){
+                scanner.nextLine();
+                System.out.println("\n"+"Lav en superhelt: ");
+
+        System.out.println("Superhelt navn?");
+        String superheltenavn = scanner.nextLine();
+
+        System.out.println("Superhelt alias?");
+        String superheltalias = scanner.nextLine();
+        System.out.println("Superheltkræfter?");
+        String superheltekræfter = scanner.nextLine();
+
+        System.out.println("Skabelsesår?");
+        int skabelsesår = scanner.nextInt();
+
+        System.out.println("Styrkeniveau?");
+        double styrkeniveau = scanner.nextDouble();
+
+        System.out.println("Højde?");
+        double højde = scanner.nextDouble();
+
+        System.out.println("Menneske?");
+        boolean menneske = scanner.nextBoolean();
+                database.createSuperhero(superheltenavn,superheltalias,superheltekræfter,skabelsesår,styrkeniveau,højde,menneske);
+
+                //System.out.println("Din superhelte navn er "+superheltenavn+"\n"+"Din superheltealias er "+superheltalias+"\n"+"Din superheltkræfter er "+superheltekræfter+"\n"+"Din superhelte skabelsesår er "+skabelsesår+"\n"+"Din superhelte styrkeniveau er "+styrkeniveau+"\n"+"Din superhelts højde er "+højde+"\n"+"Din superhelt er et menneske "+menneske);
+
+
+
+               /* System.out.println();
+                for (SuperHero superhero : database.getallhelteDatabase()){
+                    System.out.println("superhelte navn: "+superhero);
+                }*/
+
+            } else if (startside ==9) {
+                System.out.println("Lukker programmet...");
+            } else if (startside == 5) {
+                System.out.println("Liste over helte: "+"\n");
+                System.out.println(database.getallhelteDatabase());
+            }
+
+        }while(true);
+
+        /*if (startside == 9) {
+            System.out.println("Lukker programmet...");
+            *//*else if (startside ==5){
+                System.out.println("liste over helte");
+                System.out.println(database.helteDatabase);
+             *//*
+            }*/
+        }
+    }
+
+
+
+
