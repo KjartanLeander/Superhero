a9c7d7ad330d203a58c43a2b31a16abfbf6b4d81
kjartanLeander <Kjar0321@stud.kea.dk>
2022-09-15 12:09:07 +0200

    added "Ryddet op i UserInterFace"

--- src/main/java/UserInterFace.java
+++ src/main/java/UserInterFace.java
@@ -1,71 +1,88 @@
+import java.awt.*;
 import java.util.Scanner;
 import java.sql.SQLOutput;
-public class UserInterFace {
 
+public class UserInterFace {
+    Scanner scanner = new Scanner(System.in);
+    Database database = new Database();
 
     public void startProgram() {
-        Database database = new Database();
         database.createTestData();
 
-        Scanner scanner = new Scanner(System.in);
-
-
-        System.out.println("-----------------------------");
         do {
-
-            System.out.println("Velkommen til MySuperheroList");
-            System.out.println("1. Opret Superhelte");
-            System.out.println("3. Search for superhero");
-            System.out.println("5. Liste over helte");
-            System.out.println("9. Luk programmet");
+            menu();
             int startside = scanner.nextInt();
-            //scanner.nextLine();
-
             if (startside == 1) {
-                scanner.nextLine();
-                System.out.println("\n" + "Lav en superhelt: ");
-
-                System.out.println("Superhelt navn?");
-                String superheltenavn = scanner.nextLine();
-
-                System.out.println("Superhelt alias?");
-                String superheltalias = scanner.nextLine();
-                System.out.println("Superheltkræfter?");
-                String superheltekræfter = scanner.nextLine();
-
-                System.out.println("Skabelsesår?");
-                int skabelsesår = scanner.nextInt();
-
-                System.out.println("Styrkeniveau?");
-                double styrkeniveau = scanner.nextDouble();
-
-                System.out.println("Højde?");
-                double højde = scanner.nextDouble();
-
-                System.out.println("Menneske?");
-                boolean menneske = scanner.nextBoolean();
-                database.createSuperhero(superheltenavn, superheltalias, superheltekræfter, skabelsesår, styrkeniveau, højde, menneske);
-                for (SuperHero helt : database.getallhelteDatabase()) {
-                    System.out.println(helt);
-                }
-
-            } else if (startside == 9) {
-                System.out.println("Lukker programmet...");
-                System.exit(0);
-            } else if (startside == 5) {
-                System.out.println("Liste over helte: " + "\n");
-                System.out.println(database.getallhelteDatabase());
-
+                lavSuperHelt1();
             } else if (startside == 3) {
-                System.out.println("Search for Superhero: " + "\n");
-                String search = scanner.nextLine();
-
-                SuperHero superHero = database.searchFor(search);
-                System.out.println(superHero);
+                søgEfterSuperhelt3();
+            } else if (startside == 5) {
+                listeOverhelte();
+            } else if (startside == 9) {
+                lukProgrammet();
             }
 
         } while (true);
+    }
 
+    public void menu() {
+        System.out.println("Velkommen til MySuperheroList");
+        System.out.println("1. Opret Superhelte");
+        System.out.println("3. Search for superhero");
+        System.out.println("5. Liste over helte");
+        System.out.println("9. Luk programmet");
+    }
+
+    public void lavSuperHelt1() {
+
+        //scanner.nextLine();
+
+        scanner.nextLine();
+        System.out.println("\n" + "Lav en superhelt: ");
+
+        System.out.println("Superhelt navn?");
+        String superheltenavn = scanner.nextLine();
+
+        System.out.println("Superhelt alias?");
+        String superheltalias = scanner.nextLine();
+        System.out.println("Superheltkræfter?");
+        String superheltekræfter = scanner.nextLine();
+
+        System.out.println("Skabelsesår?");
+        int skabelsesår = scanner.nextInt();
+
+        System.out.println("Styrkeniveau?");
+        double styrkeniveau = scanner.nextDouble();
+
+        System.out.println("Højde?");
+        double højde = scanner.nextDouble();
+
+        System.out.println("Menneske?");
+        boolean menneske = scanner.nextBoolean();
+        database.createSuperhero(superheltenavn, superheltalias, superheltekræfter, skabelsesår, styrkeniveau, højde, menneske);
+        for (SuperHero helt : database.getallhelteDatabase())
+            System.out.println(helt);
+    }
+
+    public void søgEfterSuperhelt3() {
+
+        scanner.nextLine();
+        System.out.println("Search for Superhero: " + "\n");
+        String search = scanner.nextLine();
+
+        SuperHero superHero = database.searchFor(search);
+        System.out.println(superHero);
+    }
+
+    public void listeOverhelte() {
+        System.out.println("Liste over helte: " + "\n");
+        System.out.println(database.getallhelteDatabase());
+
+    }
+
+    public void lukProgrammet() {
+        System.out.println("Lukker programmet...");
+        System.exit(0);
     }
 }
 
